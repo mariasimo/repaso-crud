17ec3d6d0fe722055eb0de84a78779c413aa6f74
María Simó <maria@coponstudio.es>
2019-11-20 12:46:21 +0100

    Done

--- routes/coaster.routes.js
+++ routes/coaster.routes.js
@@ -33,5 +33,64 @@ router.post('/new', (req, res, next) => {
 })
 
 
+// All coasters
+router.get('/', (req,res) => {
+    Coasters
+    .find()
+    .populate('park')
+    .sort({name: 1})
+    .then(allCoasters => res.render('coasters/coasters-index', {allCoasters}))
+})
+
+
+// Coaster details
+router.get('/:id', (req, res) => {
+    Coasters
+    .findById({_id: req.params.id})
+    .populate('park')
+    .then(coasterDetails => {
+        res.render('coasters/coaster-details', {coasterDetails})
+    });
+})
+
+router.post('/delete/:id', (req,res)=>{   
+
+    Coasters
+    .findByIdAndDelete({_id:req.params.id})
+    .then(deletedCoaster => {
+        console.log(`${deletedCoaster.name} was deleted from db \n ${deletedCoaster}`)
+        res.redirect('/coasters')
+    })
+    .catch(err => console.log(`Error while deleting ${deletedCoaster.name} coaster. Error: ${err}`))
+})
+
+
+
+// Coaster details
+router.get('/edit/:id', (req, res) => {
+    let coasterToEdit;
+
+    Coasters
+    .findById({_id: req.params.id})
+    .then(coasterPayload => {
+        coasterToEdit = coasterPayload,
+        Parks.find().select({name: 1, _id: 1})
+        .then(parks => {
+        res.render('coasters/coaster-edit', {coasterToEdit, parks})
+        })
+    })
+})
+
+router.post('/edit/:id', (req,res)=>{    
+
+    Coasters
+    .findByIdAndUpdate({_id: req.params.id}, req.body)
+    .then(updatedCoaster => {
+        console.log({alert: "Coaster was updated", updatedCoaster})
+        res.redirect(`/coasters/${updatedCoaster._id}`)
+    })
+    .catch(err => console.log(`Error while updating ${deletedCoaster.name} coaster \n ${err}`))
+})
+
 
 module.exports = router
